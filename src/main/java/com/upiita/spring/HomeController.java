6bbe78c959b9d1245248d7ac0ec1099d2cc63a9f
salvador balleza <cursos@S2M29.ad.udi.upiita.ipn.mx>
2014-02-22 12:33:09 -0600

    cambio muy importante

--- src/main/java/com/upiita/spring/HomeController.java
+++ src/main/java/com/upiita/spring/HomeController.java
@@ -14,6 +14,34 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
+/**
+ * Handles requests for the application home page.
+ */
+/**
+ * Handles requests for the application home page.
+ *//**
+ * Handles requests for the application home page.
+ *//**
+ * Handles requests for the application home page.
+ */
+/**
+ * Handles requests for the application home page.
+ */
+/**
+ * Handles requests for the application home page.
+ */
+/**
+ * Handles requests for the application home page.
+ */
+/**
+ * Handles requests for the application home page.
+ */
+/**
+ * Handles requests for the application home page.
+ */
+/**
+ * Handles requests for the application home page.
+ */
 @Controller
 public class HomeController {
 	
